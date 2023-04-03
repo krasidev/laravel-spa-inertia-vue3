38aed61599e56d86bf9594bac4d4e170825b54b2
krasidev <krasidev@github.com>
2023-04-03 15:42:56 +0300

    Added/removed basic devDependencies for bootstrap in a package.json file.

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,10 @@
-window._ = require('lodash');
+import loadash from 'lodash';
+window._ = loadash;
+
+import * as Popper from '@popperjs/core';
+window.Popper = Popper;
+
+import 'bootstrap';
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
@@ -6,7 +12,8 @@ window._ = require('lodash');
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-window.axios = require('axios');
+import axios from 'axios';
+window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
@@ -18,7 +25,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 // import Echo from 'laravel-echo';
 
-// window.Pusher = require('pusher-js');
+// import Pusher from 'pusher-js';
+// window.Pusher = Pusher;
 
 // window.Echo = new Echo({
 //     broadcaster: 'pusher',
